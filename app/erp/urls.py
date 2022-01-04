2e16f86e9ddf9545b670d176c2e15385df40cb03
zachafe@gmail.com <zachafe@gmail.com>
2022-01-03 22:45:37 -0500

    video #31 viewDelete

--- app/erp/urls.py
+++ app/erp/urls.py
@@ -28,5 +28,7 @@ urlpatterns = [
     #path('category/list/',category_list,name='category_list_function'),
     path('category/list/',CategoryListView.as_view(),name='category_list'),
     #path('category/listClass2/',CategoryListView.as_view(),name='category_list2'),
-    path('category/add/',CategoryCreateView.as_view(),name='category_create')
+    path('category/add/',CategoryCreateView.as_view(),name='category_create'),
+    path('category/edit/<int:pk>/',CategoryUpdateView.as_view(),name='category_update'),
+    path('category/delete/<int:pk>/',CategoryDeleteView.as_view(),name='category_delete'),
 ]
--- app/erp/views/category/views.py
+++ app/erp/views/category/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,redirect
 from django.urls import reverse_lazy
 from django.views.decorators.csrf import csrf_exempt,csrf_protect
-from django.views.generic import ListView,CreateView
+from django.views.generic import ListView,CreateView,UpdateView,DeleteView
 from django.utils.decorators import method_decorator
 from django.http import HttpResponse, JsonResponse,HttpResponseRedirect
 
@@ -99,4 +99,52 @@ class CategoryCreateView(CreateView):
         context['list_url'] = reverse_lazy('erp:category_list')
         context['action'] = 'add'
         return context
+
+#VISTA BASADA EN CLASES UPDATE
+class CategoryUpdateView(UpdateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'category/create.html'
+    success_url = reverse_lazy('erp:category_list')
     
+    def dispatch(self, request, *args, **kwargs):
+        #OBTENER EL OBJETO ACTUAL QUE ESTAMOS LLAMANDO POR AJAX
+        self.object = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            action = request.POST['action']
+            if action == 'edit':
+                form = self.get_form()
+                data = form.save()
+            else:
+                data['error'] = 'No ha ingresado a ninguna opción'
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data)
+    
+    def get_context_data(self, **kwargs):
+        #print(self.object)
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Edicion de una Categoria'
+        context['entity'] = 'Categorias'
+        context['list_url'] = reverse_lazy('erp:category_list')
+        context['action'] = 'edit'
+        return context
+
+#VISTA BASADA EN CLASES DELETE
+class CategoryDeleteView(DeleteView):
+    model = Category
+    template_name = 'category/delete.html'
+    success_url = reverse_lazy('erp:category_list')
+    
+    def get_context_data(self, **kwargs):
+        #print(self.object)
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Eliminacion de una Categoria'
+        context['entity'] = 'Categorias'
+        context['list_url'] = reverse_lazy('erp:category_list')
+        context['action'] = 'delete'
+        return context
