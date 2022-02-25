8802fadc5ee70aebe6693ffb2534b957542396e0
afquintero_pc_safe <zachafe@gmail.com>
2022-02-25 17:17:37 -0500

    video 75

--- app/erp/views/dashboard/views.py
+++ app/erp/views/dashboard/views.py
@@ -1,25 +1,74 @@
-from django.db.models import Sum
-from django.db.models.functions import Coalesce
-from django.views.generic import TemplateView
 from datetime import datetime
 
-from app.erp.models import Sale
+from django.db.models import Sum
+from django.db.models.functions import Coalesce
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import TemplateView
+
+from app.erp.models import Sale, Product, DetSale
 
 
 class DashboardView(TemplateView):
     template_name = 'dashboard.html'
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            action = request.POST['action']
+            if action == 'get_graph_sales_year_month':
+                data  = {
+                    'name': 'Porcentaje de venta',
+                    'showInLegend': False,
+                    'colorByPoint': True,
+                    'data': self.get_graph_sales_year_month()
+                }
+            elif action == 'get_graph_sales_products_year_month':
+                data = {
+                    'name': 'Porcentaje',
+                    'colorByPoint': True,
+                    'data': self.get_graph_sales_products_year_month(),
+                }
+            else:
+                data['error'] = 'Ha ocurrido un error'
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data, safe=False)
 
     def get_graph_sales_year_month(self):
         data = []
         try:
             year = datetime.now().year
             for m in range(1, 13):
-                #se debe validar Coalesce que es una funcion que indica que si el valor es none retorna 0
                 #total = Sale.objects.filter(date_joined__year=year, date_joined__month=m).aggregate(r=Coalesce(Sum('total'), 0)).get('r')
                 total = Sale.objects.filter(date_joined__year=year,date_joined__month=m).aggregate(r=Sum('total')).get('r')
                 if total is None:
                     total = 0
                 data.append(float(total))
+        except:
+            pass
+        return data
+
+    def get_graph_sales_products_year_month(self):
+        data = []
+        year = datetime.now().year
+        month = datetime.now().month
+        try:
+            for p in Product.objects.all():
+                #total = DetSale.objects.filter(sale__date_joined__year=year, sale__date_joined__month=month, prod_id=p.id).aggregate(r=Coalesce(Sum('subtotal'), 0)).get('r')
+                total = DetSale.objects.filter(sale__date_joined__year=year,sale__date_joined__month=month,prod_id=p.id).aggregate(r=Sum('subtotal')).get('r')
+                if total is None:
+                    total = 0
+                if total > 0:
+                    data.append({
+                        'name': p.name,
+                        'y': float(total)
+                    })
         except:
             pass
         return data
