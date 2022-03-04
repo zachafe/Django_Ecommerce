2b6c1837d6ffbb39f3132844f569bc26e5eabc04
afquintero_pc_safe <zachafe@gmail.com>
2022-03-04 08:25:47 -0500

    Video 87 varios permisos en un grupo

--- app/erp/mixins.py
+++ app/erp/mixins.py
@@ -21,12 +21,13 @@ class ValidatePermissionRequiredMixin(object):
     permission_required = ''
     url_redirect = None
 
-    # def get_perms(self):
-    #     if isinstance(self.permission_required, str):
-    #         perms = (self.permission_required,)
-    #     else:
-    #         perms = self.permission_required
-    #     return perms
+    def get_perms(self):
+        perms = []
+        if isinstance(self.permission_required, str):
+            perms.append(self.permission_required)
+        else:
+            perms = list(self.permission_required)
+        return perms
 
     def get_url_redirect(self):
         if self.url_redirect is None:
@@ -35,11 +36,17 @@ class ValidatePermissionRequiredMixin(object):
 
     def dispatch(self, request, *args, **kwargs):
         request = get_current_request()
+        #para que el super user mire todo
+        if request.user.is_superuser:
+            return super().dispatch(request, *args, **kwargs)
         if 'group' in request.session:
             group = request.session['group']
-            #group = Group.objects.get(pk=1)
-            if group.permissions.filter(codename=self.permission_required):
-                return super().dispatch(request, *args, **kwargs)
+            perms = self.get_perms()
+            for p in perms:
+                if not group.permissions.filter(codename=p).exists():
+                    messages.error(request, 'No tiene permiso para ingresar a este módulo')
+                    return HttpResponseRedirect(self.get_url_redirect())
+            return super().dispatch(request, *args, **kwargs)
         messages.error(request, 'No tiene permiso para ingresar a este módulo')
         return HttpResponseRedirect(self.get_url_redirect())
   
--- app/erp/views/product/views.py
+++ app/erp/views/product/views.py
@@ -18,6 +18,8 @@ class ProductListView(LoginRequiredMixin, ValidatePermissionRequiredMixin, ListV
     #permission_required = 'erp.view_product'
     #permiso por perfil
     permission_required = 'view_product'
+    #permisos combinados
+    #permission_required = 'view_product', 'change_product', 'delete_product', 'add_product'
 
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
