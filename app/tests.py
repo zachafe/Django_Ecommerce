f875c6606cdfa6c8335e258284312b916f40ca03
afquintero_pc_safe <zachafe@gmail.com>
2022-03-07 08:28:05 -0500

    Video 91 renderizar HTML para enviar por Correo

--- app/tests.py
+++ app/tests.py
@@ -1,9 +1,14 @@
+from ecommerce.wsgi import *
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+from django.template.loader import render_to_string
 
 from ecommerce import settings
+from app.user.models import User
 
-
+#FUNCION DE ENVIO DE CORREO DE PRUEBA
 def send_email():
     try:
         mailServer = smtplib.SMTP(settings.EMAIL_HOST, settings.EMAIL_PORT)
@@ -15,10 +20,16 @@ def send_email():
 
         email_to = 'zachafe@gmail.com'
         # Construimos el mensaje simple
-        mensaje = MIMEText("""Este es el mensaje de prueba Django APP afquintero""")
+        #mensaje = MIMEText("""Este es el mensaje de prueba Django APP afquintero""")
+        #MENAJES COMPUESTO
+        mensaje = MIMEMultipart()
         mensaje['From'] = settings.EMAIL_HOST_USER
         mensaje['To'] = email_to
         mensaje['Subject'] = "Tienes un correo"
+
+        #CONTENIDO CUANDO EL MENSAJE ES COMPUESTO 
+        content = render_to_string('send_email.html', {'user': User.objects.get(pk=1)})
+        mensaje.attach(MIMEText(content, 'html'))
 
         mailServer.sendmail(settings.EMAIL_HOST_USER,
                             email_to,
